6caa1a371bf1e4f15ab796d342073de025562e2b
serpient <littlegangwolf@gmail.com>
2019-02-15 16:56:57 -0800

    add jquery and vanilla JS dom manipulation example

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,21 @@
-const testerJs = () => {
-  $(document).ready(() => {
-    $('.subtext').css('color', 'red');
-    $('.subtext').css('background-color', 'blue');
-  })
+const testerPureJavaScriptFunction = () => {
+  const subtext = document.getElementsByClassName('subtext')[0];
+  subtext.setAttribute('style', "background-color: blue" );
 }
 
-testerJs();
+
+window.addEventListener('onload', () => {
+  console.log('loading')
+  testerPureJavaScriptFunction();
+})
+
+
+// const testerJQueryFunction = () => {
+//   $(document).ready(() => {
+//       $('.subtext').css('color', 'red');
+//       $('.subtext').css('background-color', 'blue');
+//   })
+// }
+
+// testerJQueryFunction();
+
