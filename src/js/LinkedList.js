b012b1b0e6373f98656de257b3680ea9bd7d8760
serpient <littlegangwolf@gmail.com>
2019-02-15 17:27:55 -0800

    starter code for LinkedList class

---
+++ src/js/LinkedList.js
@@ -0,0 +1,25 @@
+class LinkedList {
+  constructor(list) {
+    this.list = list;
+  }
+  pop = () => {
+    // removes last element from list
+  }
+  push = () => {
+    // adds element to end of list
+  }
+  shift = () => {
+    // removes first item in list
+  }
+  unshift = () => {
+    // adds item to beginning of list 
+  }
+  sort = () => {
+    // sorts list
+  }
+  includes = () => {
+    // checks to see if an item is included in the list
+  }
+}
+
+export default LinkedList;
