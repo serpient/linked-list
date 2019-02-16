60d22e6e3d1df78c2cc3e847f327e5f1fe4a085e
serpient <littlegangwolf@gmail.com>
2019-02-15 16:56:40 -0800

    add names list

---
+++ src/js/names.js
@@ -0,0 +1,304 @@
+const names = [
+    {
+      "name": "Harmon"
+    },
+    {
+      "name": "Austin"
+    },
+    {
+      "name": "Angelica"
+    },
+    {
+      "name": "Robert"
+    },
+    {
+      "name": "Christine"
+    },
+    {
+      "name": "Luz"
+    },
+    {
+      "name": "Florine"
+    },
+    {
+      "name": "Marshall"
+    },
+    {
+      "name": "Leonor"
+    },
+    {
+      "name": "Kristy"
+    },
+    {
+      "name": "Tammy"
+    },
+    {
+      "name": "Keller"
+    },
+    {
+      "name": "Maura"
+    },
+    {
+      "name": "Willie"
+    },
+    {
+      "name": "Craig"
+    },
+    {
+      "name": "Donovan"
+    },
+    {
+      "name": "Josefa"
+    },
+    {
+      "name": "Wendy"
+    },
+    {
+      "name": "Kline"
+    },
+    {
+      "name": "Chandra"
+    },
+    {
+      "name": "Singleton"
+    },
+    {
+      "name": "Leah"
+    },
+    {
+      "name": "Clemons"
+    },
+    {
+      "name": "Ines"
+    },
+    {
+      "name": "Cochran"
+    },
+    {
+      "name": "Meredith"
+    },
+    {
+      "name": "Janet"
+    },
+    {
+      "name": "Kimberley"
+    },
+    {
+      "name": "Anna"
+    },
+    {
+      "name": "Fanny"
+    },
+    {
+      "name": "Ethel"
+    },
+    {
+      "name": "Bird"
+    },
+    {
+      "name": "Celia"
+    },
+    {
+      "name": "Mayer"
+    },
+    {
+      "name": "Monroe"
+    },
+    {
+      "name": "Tillman"
+    },
+    {
+      "name": "Oliver"
+    },
+    {
+      "name": "Fuller"
+    },
+    {
+      "name": "Frances"
+    },
+    {
+      "name": "Adrienne"
+    },
+    {
+      "name": "Ferrell"
+    },
+    {
+      "name": "Ware"
+    },
+    {
+      "name": "Potts"
+    },
+    {
+      "name": "Faye"
+    },
+    {
+      "name": "Buchanan"
+    },
+    {
+      "name": "Rutledge"
+    },
+    {
+      "name": "Kelly"
+    },
+    {
+      "name": "Chelsea"
+    },
+    {
+      "name": "Sherri"
+    },
+    {
+      "name": "Flynn"
+    },
+    {
+      "name": "Bean"
+    },
+    {
+      "name": "Hughes"
+    },
+    {
+      "name": "Lee"
+    },
+    {
+      "name": "Anderson"
+    },
+    {
+      "name": "Arlene"
+    },
+    {
+      "name": "Parrish"
+    },
+    {
+      "name": "Iris"
+    },
+    {
+      "name": "Ola"
+    },
+    {
+      "name": "Debora"
+    },
+    {
+      "name": "Chapman"
+    },
+    {
+      "name": "Foreman"
+    },
+    {
+      "name": "Alexandria"
+    },
+    {
+      "name": "Priscilla"
+    },
+    {
+      "name": "Frankie"
+    },
+    {
+      "name": "Rosalyn"
+    },
+    {
+      "name": "Lynette"
+    },
+    {
+      "name": "Leann"
+    },
+    {
+      "name": "Wooten"
+    },
+    {
+      "name": "Iva"
+    },
+    {
+      "name": "Stephanie"
+    },
+    {
+      "name": "Cynthia"
+    },
+    {
+      "name": "Andrea"
+    },
+    {
+      "name": "Browning"
+    },
+    {
+      "name": "Bailey"
+    },
+    {
+      "name": "Noreen"
+    },
+    {
+      "name": "Candace"
+    },
+    {
+      "name": "Hanson"
+    },
+    {
+      "name": "Barbara"
+    },
+    {
+      "name": "Patel"
+    },
+    {
+      "name": "Reyna"
+    },
+    {
+      "name": "Lyons"
+    },
+    {
+      "name": "Socorro"
+    },
+    {
+      "name": "Mooney"
+    },
+    {
+      "name": "Ellis"
+    },
+    {
+      "name": "Kerry"
+    },
+    {
+      "name": "Dejesus"
+    },
+    {
+      "name": "Saunders"
+    },
+    {
+      "name": "Magdalena"
+    },
+    {
+      "name": "Dillard"
+    },
+    {
+      "name": "Gertrude"
+    },
+    {
+      "name": "Colleen"
+    },
+    {
+      "name": "Hatfield"
+    },
+    {
+      "name": "Whitehead"
+    },
+    {
+      "name": "Hillary"
+    },
+    {
+      "name": "George"
+    },
+    {
+      "name": "Rhoda"
+    },
+    {
+      "name": "Johanna"
+    },
+    {
+      "name": "Mcdowell"
+    },
+    {
+      "name": "Farley"
+    },
+    {
+      "name": "Betty"
+    }
+  ]
+
+  export default names;
