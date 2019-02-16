379291df762231310fdc1230e7b57d976ac09984
serpient <littlegangwolf@gmail.com>
2019-02-15 17:28:08 -0800

    remove starter  function; update onload to load

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,9 @@
+// DOM Manipulation with Pure JavaScript
 const testerPureJavaScriptFunction = () => {
   const subtext = document.getElementsByClassName('subtext')[0];
   subtext.setAttribute('style', "background-color: blue" );
 }
 
-
-window.addEventListener('onload', () => {
-  console.log('loading')
+window.addEventListener('load', () => {
   testerPureJavaScriptFunction();
 })
-
-
-// const testerJQueryFunction = () => {
-//   $(document).ready(() => {
-//       $('.subtext').css('color', 'red');
-//       $('.subtext').css('background-color', 'blue');
-//   })
-// }
-
-// testerJQueryFunction();
-
